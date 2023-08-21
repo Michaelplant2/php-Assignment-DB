0aef6b23d4522b8d9d2d945bd587b6230e00ff2c
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2023-08-21 12:11:10 -0400

    Update assignments_db.php

--- model/assignments_db.php
+++ model/assignments_db.php
@@ -3,15 +3,14 @@
    function get_assignments_by_course($course_id) {
       global $db;
       if ($course_id) {
-         $query = "SELECT A.ID, A.Description, C.courseName FROM assignments A LEFT JOIN 
-         courses C ON A.courseID = C.courseID WHERE A.courseID = :course_id ORDER BY A.ID";
-         $statement = $db->prepare($query);
-         $statement->bindValue(':course_id', $course_id);
+          $query = 'SELECT A.ID, A.Description, C.courseName FROM assignments A LEFT JOIN courses C ON A.courseID = C.courseID WHERE A.courseID = :course_id ORDER BY ID';
       } else {
-         $query = "SELECT A.ID, A.Description, C.courseName FROM assignments A LEFT JOIN 
-         courses C ON A.courseID = C.courseID ORDER BY C.courseID";
+          $query = 'SELECT A.ID, A.Description, C.courseName FROM assignments A LEFT JOIN courses C ON A.courseID = C.courseID ORDER BY C.courseID';
       }
       $statement = $db->prepare($query);
+      if ($course_id) {
+          $statement->bindValue(':course_id', $course_id);
+      }
       $statement->execute();
       $assignments = $statement->fetchAll();
       $statement->closeCursor();
